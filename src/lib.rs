0a3f0c07516fbcf701dc060cc7dcb0437b921723
k0pernicus <antonin.carette@gmail.com>
2016-07-25 20:44:16 -0400

    Add Rust stuff to call Rust code via boot.asm

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![feature(lang_items)]
+#![no_std]
+
+#[lang = "eh_personality"]
+extern fn eh_personality() {
+}
+
+#[lang = "panic_fmt"]
+extern fn rust_begin_panic() -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern fn kernel_main() -> ! {
+    unsafe {
+        let vga = 0xb8000 as *mut u64;
+
+        *vga = 0x2f592f412f4b2f4f;
+    };
+
+    loop {}
+}
