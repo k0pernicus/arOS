63f7554b05cf8a9367358cd4983d57b282bc681f
k0pernicus <antonin.carette@gmail.com>
2018-03-18 17:16:45 +0100

    Change the behaviour of arOS lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,21 @@
 #![feature(lang_items)]
 #![no_std]
 
-static mut VGA: u64 = 0xb8000;
-static COLOR: u64 = 0x02;
+extern crate vga;
 
-macro_rules! k_println {
-    ($e:expr) => {
-        unsafe {
-            let mut offset : u64 = 0;
-            for c in $e.chars() {
-                *((VGA + offset) as *mut u64) = c as u64;
-                offset += 1;
-                *((VGA + offset) as *mut u64) = COLOR;
-                offset += 1;
-            }
-            *((VGA + offset) as *mut u64) = 0x0A;
-            VGA = VGA + 160;
-        }
-    };
-}
+use vga::buffer::print_message;
 
 #[lang = "eh_personality"]
-extern fn eh_personality() {
-}
+extern "C" fn eh_personality() {}
 
 #[lang = "panic_fmt"]
-extern fn rust_begin_panic() -> ! {
+extern "C" fn rust_begin_panic() -> ! {
     loop {}
 }
 
 #[no_mangle]
-pub extern fn kernel_main() -> ! {
-    // k_println!("Hello");
-    k_println!("arOS - another rust Operating System");
-    k_println!("system version 0.0.1");
-    k_println!("");
-    k_println!("Welcome user!");
-
+pub extern "C" fn kernel_main() -> ! {
+    // Test
+    print_message();
     loop {}
 }
